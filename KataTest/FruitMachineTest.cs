aa881e20726e3701b5324aa2f3146d0d5ae881d2
lily.yen79 <lily.yen79@TI03NB61>
2017-12-02 20:48:01 +0800

    add test case, and kata test case

--- KataTest/FruitMachineTest.cs
+++ KataTest/FruitMachineTest.cs
@@ -100,5 +100,51 @@ namespace KataTest
 
             Assert.AreEqual(18, result);
         }
+
+        [Test]
+        public void ThreeWild_chaosReals()
+        {
+            string[] reel1 = new string[] { "King", "Jack", "Wild", "Bell", "Star", "Seven", "Queen", "Cherry", "Shell", "Bar" };
+            string[] reel2 = new string[] { "Star", "Bar", "Jack", "Seven", "Queen", "Wild", "King", "Bell", "Cherry", "Shell" };
+            string[] reel3 = new string[] { "King", "Bell", "Jack", "Shell", "Star", "Cherry", "Queen", "Bar", "Wild", "Seven" };
+            reels = new List<string[]> { reel1, reel2, reel3 };
+
+            var result = _class.Fruit(reels, new int[] { 2, 5, 8 });
+
+            Assert.AreEqual(100, result);
+        }
+
+        [Test]
+        public void kataTest_2()
+        {
+            string[] reel1 = new string[] { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
+            string[] reel2 = new string[] { "Bar", "Wild", "Queen", "Bell", "King", "Seven", "Cherry", "Jack", "Star", "Shell" };
+            string[] reel3 = new string[] { "Bell", "King", "Wild", "Bar", "Seven", "Jack", "Shell", "Cherry", "Queen", "Star" };
+            reels = new List<string[]> { reel1, reel2, reel3 };
+            int[] spins = new int[] { 5, 4, 3 };
+            Assert.AreEqual(0, _class.Fruit(reels, spins));
+        }
+
+        [Test]
+        public void kataTest_3()
+        {
+            string[] reel1 = new string[] { "King", "Cherry", "Bar", "Jack", "Seven", "Queen", "Star", "Shell", "Bell", "Wild" };
+            string[] reel2 = new string[] { "Bell", "Seven", "Jack", "Queen", "Bar", "Star", "Shell", "Wild", "Cherry", "King" };
+            string[] reel3 = new string[] { "Wild", "King", "Queen", "Seven", "Star", "Bar", "Shell", "Cherry", "Jack", "Bell" };
+            reels = new List<string[]> { reel1, reel2, reel3 };
+            int[] spins = new int[] { 0, 0, 1 };
+            Assert.AreEqual(3, _class.Fruit(reels, spins));
+        }
+
+        [Test]
+        public void kataTest_4()
+        {
+            string[] reel1 = new string[] { "King", "Jack", "Wild", "Bell", "Star", "Seven", "Queen", "Cherry", "Shell", "Bar" };
+            string[] reel2 = new string[] { "Star", "Bar", "Jack", "Seven", "Queen", "Wild", "King", "Bell", "Cherry", "Shell" };
+            string[] reel3 = new string[] { "King", "Bell", "Jack", "Shell", "Star", "Cherry", "Queen", "Bar", "Wild", "Seven" };
+            reels = new List<string[]> { reel1, reel2, reel3 };
+            int[] spins = new int[] { 0, 5, 0 };
+            Assert.AreEqual(6, _class.Fruit(reels, spins));
+        }
     }
 }
