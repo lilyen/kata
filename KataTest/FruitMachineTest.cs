542365a94ec7c374083ebde64e59b29915e0ebd5
lily.yen79 <lily.yen79@TI03NB61>
2017-12-02 14:35:31 +0800

    creat FruitMachine class

---
+++ KataTest/FruitMachineTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using kata;
+
+namespace KataTest
+{
+    /// <summary>
+    /// Summary description for FruitMachine
+    /// </summary>
+    [TestFixture]
+    public class FruitMachineTest
+    {
+        FruitMachine testClass = new FruitMachine();
+        [Test]
+        public void NoMatch()
+        {
+            string[] reel = { "Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack" };
+            List<string[]> reels = new List<string[]> { reel, reel, reel };
+
+            int[] spins = { 0, 1, 2};
+
+            Assert.AreEqual(0, testClass.Fruit(reels, spins));
+        }
+    }
+}
---
+++ kata/FruitMachine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata
+{
+    public class FruitMachine
+    {
+        public int Fruit(List<string[]> reels, int[] spins)
+        {
+            return 0;
+        }
+    }
+}
