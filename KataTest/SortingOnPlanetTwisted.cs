22bd94ac01cdbc69e4fc8b4aeda8933c7d75c010
lily.yen79 <lily.yen79@TI03NB61>
2017-12-09 22:38:44 +0800

    sort array ignore twist 3 and 7

---
+++ KataTest/SortingOnPlanetTwisted.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using kata;
+
+namespace KataTest
+{
+    [TestFixture]
+    class SortingOnPlanetTwistedTest
+    {
+        private SortingOnPlanetTwisted target = new SortingOnPlanetTwisted();
+
+        [Test]
+        public void sortWithNoTwist()
+        {
+            Assert.AreEqual(string.Join(",", new[] { 1, 2, 4, 5, 6, 8, 9 }), string.Join(",", target.SortTwisted37(new[] { 1, 2, 4, 5, 6, 8, 9 })));
+            Assert.AreEqual(string.Join(",", new[] { 1, 2, 4, 5, 6, 8, 9 }), string.Join(",", target.SortTwisted37(new[] { 1, 4, 5, 2, 9, 8, 6 })));
+        }
+
+        [Test]
+        public void KataTests()
+        {
+            Assert.AreEqual(string.Join(",", new[] { 1, 2, 7, 4, 5, 6, 3, 8, 9 }), string.Join(",", target.SortTwisted37(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 })));
+            Assert.AreEqual(string.Join(",", new[] { 12, 14, 13 }), string.Join(",", target.SortTwisted37(new[] { 12, 13, 14 })));
+            Assert.AreEqual(string.Join(",", new[] { 2, 7, 4, 3, 9 }), string.Join(",", target.SortTwisted37(new[] { 9, 2, 4, 7, 3 })));
+        }
+
+    }
+}
---
+++ kata/SortingOnPlanetTwisted.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata
+{
+//Sorting on planet Twisted-3-7
+    public class SortingOnPlanetTwisted
+    {
+        public int[] SortTwisted37(int[] array)
+        {
+            Array.Sort(array);
+            return array;
+        }
+    }
+}
