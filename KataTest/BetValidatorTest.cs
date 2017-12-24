2414bf601c01873be9a7fe81a1f9d4e3c1fb401c
lily.yen79 <lily.yen79@TI03NB61>
2017-12-24 13:04:07 +0800

    separate string to int array

---
+++ KataTest/BetValidatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using kata;
+
+namespace KataTest
+{
+    [TestFixture]
+    class BetValidatorTest
+    {
+        private BetValidator target = new BetValidator();
+
+        [Test]
+        public void SeparatedbySingalCharacter()
+        {
+            var expected = new int[] {1, 2, 3, 4, 5};
+
+            var result = target.ValidateBet(5, 90, "1 2 3 4 5");
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ kata/BetValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata
+{
+    public class BetValidator
+    {
+        public int[] ValidateBet(int N, int M, string text)
+        {
+            var separateResult = text.Split(' ');
+            var result = new List<int>();
+
+            foreach (var str in separateResult)
+            {
+                result.Add(Int32.Parse(str));
+            }
+
+            return result.ToArray();
+        }
+    }
+}
