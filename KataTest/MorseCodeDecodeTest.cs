5b8f6c23357bb9472c079e12ef4fc62393d62081
lily.yen79 <lily.yen79@TI03NB61>
2017-12-24 12:44:41 +0800

    transfort morse code

---
+++ KataTest/MorseCodeDecodeTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kata;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace KataTest
+{
+    [TestFixture]
+    class MorseCodeDecodeTest
+    {
+        private MorseCodeDecode target = new MorseCodeDecode();
+        [Test]
+        public void simple()
+        {
+            try
+            {
+                string input = ".... . -.--   .--- ..- -.. .";
+                string expected = "HEY JUDE";
+
+                string actual = target.Decode(input);
+
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("There seems to be an error somewhere in your code. Exception message reads as follows: " + ex.Message);
+            }
+        }
+
+        [Test]
+        public void testSOS()
+        {
+            try
+            {
+                string input = "...---...";
+                string expected = "SOS";
+
+                string actual = target.Decode(input);
+
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("There seems to be an error somewhere in your code. Exception message reads as follows: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ kata/MorseCodeDecode.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata
+{
+    //https://www.codewars.com/kata/decode-the-morse-code/train/csharp
+    public class MorseCodeDecode
+    {
+        Dictionary<string, String> morse = new Dictionary<string, String>()
+        {
+            {".-", "A"},
+            {"-...", "B"},
+            {"-.-.", "C"},
+            {"-..", "D"},
+            {".", "E"},
+            {"..-.", "F"},
+            {"--.", "G"},
+            {"....", "H"},
+            {"..", "I"},
+            {".---", "J"},
+            {"-.-", "K"},
+            {".-..", "L"},
+            {"--", "M"},
+            {"-.", "N"},
+            {"---", "O"},
+            {".--.", "P"},
+            {"--.-", "Q"},
+            {".-.", "R"},
+            {"...", "S"},
+            {"-", "T"},
+            {"..-", "U"},
+            {"...-", "V"},
+            { ".--", "W"},
+            {"-..-", "X"},
+            {"-.--", "Y"},
+            {"--..", "Z"},
+            {"-----", "0"},
+            {".----", "1"},
+            {"..---", "2"},
+            {"...--", "3"},
+            {"....-", "4"},
+            {".....", "5"},
+            { "-....", "6"},
+            { "--...", "7"},
+            {"---..", "8"},
+            {"----.", "9"},
+            {"...---...", "SOS" }
+        };
+
+        public string Decode(string morseCode)
+        {
+            var result = "";
+            var words = morseCode.Split(' ');
+            var speaceNum = 0;
+            foreach (var word in words)
+            {
+                if (word != "")
+                {
+                    result += morse[word];
+                }
+                else
+                {
+                    speaceNum++;
+                    if (speaceNum >= 2)
+                    {
+                        result += " ";
+                        speaceNum = 0;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
