4a1bbef7f98c6bc219b831b89d9702d330c97160
lily.yen79 <lily.yen79@TI03NB61>
2018-01-14 23:43:35 +0800

    ignore need abbreviator tet case

--- KataTest/AbbreviatorTest.cs
+++ KataTest/AbbreviatorTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using kata;
 using NUnit.Framework;
 
 namespace KataTest
@@ -10,5 +11,15 @@ namespace KataTest
     [TestFixture]
     class AbbreviatorTest
     {
+        Abbreviator target = new Abbreviator();
+
+        [Ignore("next")]
+        public void Abbreviator_Singal_Word()
+        {
+            var expected = "i18n";
+            var reault = target.Abbreviate("internationalization");
+
+            Assert.Equals(expected, reault);
+        }
     }
 }
