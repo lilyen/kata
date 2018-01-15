66957ac0ae462721040052826aa4acdfd4c64d34
lily.yen79 <lily.yen79@TI03NB61>
2018-01-16 00:45:32 +0800

    test case add other sentence

--- KataTest/AbbreviatorTest.cs
+++ KataTest/AbbreviatorTest.cs
@@ -32,6 +32,7 @@ namespace KataTest
 
         [TestCase("this is an apple", "t2s is an a3e", TestName = "only use space disting word")]
         [TestCase("elephant-rides are really fun!", "e6t-r3s are r4y fun!", TestName = "use diff disting word, end sentence has punctuation")]
+        [TestCase("my. dog, isn't feeling very well.", "my. dog, isn't f5g v2y w2l.", TestName = "use diff disting word, end sentence has punctuation")]
         public void Abbreviator_Sentence(string sentence, string expected)
         {
             var result = target.Abbreviate(sentence);
