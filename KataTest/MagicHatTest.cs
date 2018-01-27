a52f89dc006f5e60ffe26230e3a956e7ab200f89
lily.yen79 <lily.yen79@TI03NB61>
2018-01-28 06:18:51 +0800

    add new class MagicHat add testmethod only one black ball

---
+++ KataTest/MagicHatTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using kata;
+using Assert = NUnit.Framework.Assert;
+
+namespace KataTest
+{
+    [TestFixture]
+    class MagicHatTest
+    {
+        [TestMethod]
+        public void OneBlackBall()
+        {
+            MagicHat target = new MagicHat();
+            string result = target.NotSoRandom(1, 0);
+
+            Assert.AreEqual("Black", result);
+        }
+    }
+}
---
+++ kata/MagicHat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata
+{
+    //https://www.codewars.com/kata/simple-fun-number-158-not-so-randoms
+    public class MagicHat
+    {
+        public string NotSoRandom(double black, double white)
+        {
+            return "Black";
+        }
+    }
+}
