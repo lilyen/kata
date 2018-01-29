15dda4116d239f8d50527e82fa5583f3dac3209e
lily.yen79 <lily.yen79@TI03NB61>
2018-01-29 23:00:36 +0800

    extract method

--- kata/MagicHat.cs
+++ kata/MagicHat.cs
@@ -3,33 +3,51 @@
     //https://www.codewars.com/kata/simple-fun-number-158-not-so-randoms
     public class MagicHat
     {
+        private double _whiteMarbles;
+        private double _blackMarbles;
+
         public string NotSoRandom(double black, double white)
         {
-            var blackMarbles = black;
-            var whiteMarbles = white;
-            if (blackMarbles == 2)
+            _blackMarbles = black;
+            _whiteMarbles = white;
+            if (_blackMarbles == 2)
             {
-                blackMarbles -= 2;
-                whiteMarbles += 1;
+                RemoveTwoBlackMarbles();
             }
-            else if(whiteMarbles == 2)
+            else if(_whiteMarbles == 2)
             {
-                whiteMarbles -= 1;
-            } else if (blackMarbles == 1 && whiteMarbles == 1)
+                RemoveTwoWhiteMarbles();
+            } else if (_blackMarbles == 1 && _whiteMarbles == 1)
             {
-                whiteMarbles -= 1;
+                RemoveTwoDifMarbles();
             }
             
-            if (blackMarbles == 1 && blackMarbles+ whiteMarbles == 1)
+            if (_blackMarbles == 1 && _blackMarbles+ _whiteMarbles == 1)
             {
                 return "Black";
             }
-            else if (whiteMarbles == 1 && blackMarbles + whiteMarbles == 1)
+            else if (_whiteMarbles == 1 && _blackMarbles + _whiteMarbles == 1)
             {
                 return "White";
             }
 
             return "Unsure";
+        }
+
+        private void RemoveTwoBlackMarbles()
+        {
+            _blackMarbles -= 2;
+            _whiteMarbles += 1;
+        }
+
+        private void RemoveTwoWhiteMarbles()
+        {
+            _whiteMarbles -= 1;
+        }
+
+        private void RemoveTwoDifMarbles()
+        {
+            _whiteMarbles -= 1;
         }
     }
 }
