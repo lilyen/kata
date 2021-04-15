03afecc513dc10430f07ce0f328c99a144ec3b54
lily <lily.yen79@TI03BY60>
2021-04-15 22:40:51 +0800

    WeightSort

---
+++ KataTest/WeightSortTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using kata;
+using NUnit.Framework;
+
+namespace KataTest
+{
+    [TestFixture]
+    class WeightSortTest
+    {
+        private WeightSort _weightSort;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _weightSort = new WeightSort();
+        }
+
+        [Test]
+        public void RemoveWhiteList()
+        {
+            var result = _weightSort.orderWeight("123   123");
+            Assert.AreEqual("123 123",result);
+        }
+
+        [Test]
+        public void FirstUseSumCompare()
+        {
+            var result = _weightSort.orderWeight("99 100");
+            Assert.AreEqual("100 99", result);
+        }
+
+        [Test]
+        public void SumSamUseStringCompare()
+        {
+            var result = _weightSort.orderWeight("90 180");
+            Assert.AreEqual("180 90", result);
+        }
+    }
+}
---
+++ kata/WeightSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace kata
+{
+    public class WeightSort
+    {
+        public string orderWeight(string strng)
+        {
+            var weights = Regex.Split(strng, "\\s+");
+            Array.Sort(weights,new WeightCompare());
+            return string.Join(" ", weights);
+        }
+    }
+
+    public class WeightCompare : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            var xSum = x.ToCharArray().Sum(a=>int.Parse(a.ToString()));
+            var ySum = y.ToCharArray().Sum(a=> int.Parse(a.ToString()));
+
+            if (xSum>ySum)
+            {
+                return 1;
+            }
+
+            if(xSum < ySum)
+            {
+                return -1;
+            }
+
+            return x.CompareTo(y);
+        }
+    }
+}
