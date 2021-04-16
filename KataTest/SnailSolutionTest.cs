2a3ce20b1a6a39436982e2402a289655d7de82c1
lily <lily.yen79@TI03BY60>
2021-04-16 17:02:09 +0800

    Snail

---
+++ KataTest/SnailSolutionTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kata;
+using NUnit.Framework;
+
+namespace KataTest
+{
+    [TestFixture]
+    class SnailSolutionTest
+    {
+        [Test]
+        public void zieo()
+        {
+            var result = new SnailSolution().Snail(new[]{new int[]{}});
+            Assert.AreEqual("1", string.Join(",", result));
+        }
+        [Test]
+        public void one()
+        {
+            var result = new SnailSolution().Snail(new []{
+                new[] { 1 },
+            });
+            Assert.AreEqual("1",string.Join(",",result));
+        }
+
+        [Test]
+        public void three()
+        {
+            var result = new SnailSolution().Snail(new[]{
+                new[] { 1, 2, 3 },
+                new[] { 4, 5, 6 },
+                new[] { 7, 8, 9 }
+            });
+            Assert.AreEqual("1,2,3,6,9,8,7,4,5", string.Join(",", result));
+        }
+    }
+}
---
+++ kata/SnailSolution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata
+{
+    //https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/solutions/csharp
+    public class SnailSolution
+    {
+        public int[] Snail(int[][] array)
+        {
+            var xSite = 0;
+            var ySite = 0;
+            var length = array[0].Length;
+            var isMinus = false;
+            var result= new List<int>();
+            Console.WriteLine(length);
+            if (length == 0)
+            {
+                return new int[]{};
+            }
+            for (int i = length-1; i >= 0; i--)
+            {
+                result.Add(array[xSite][ySite]);
+                if(i!=0) ySite++;
+            }
+            for (int i = length - 2; i >= 0; i--)
+            {
+                for (int a = i; a >= 0; a--)
+                {
+                    if (isMinus)
+                    {
+                        xSite--;
+                    }
+                    else
+                    {
+                        xSite++;
+                    }
+                    result.Add(array[xSite][ySite]);
+                }
+
+                for (int a = i; a >= 0; a--)
+                {
+                    if (!isMinus)
+                    {
+                        ySite--;
+                    }
+                    else
+                    {
+                        ySite++;
+                    }
+                    result.Add(array[xSite][ySite]);
+                }
+                isMinus = !isMinus;
+            }
+            return result.ToArray();
+        }
+    }
+}
